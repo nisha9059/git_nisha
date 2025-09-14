52d6db80f34e9220befc17ae696c7753d2a319ac
Choudhary <nisha.271433@gmail.com>
2025-09-14 20:31:11 +0530

    bash1

--- Bash/terraform.sh
+++ Bash/terraform.sh
@@ -2,20 +2,8 @@
 
 echo "Installing the Terraform on AWS"
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-sudo apt update
-
-sudo apt-get install terraform
+sudo yum update -y && sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo 
+sudo yum install -y terraform && terraform -v
 
 echo "Installed Terraform"
